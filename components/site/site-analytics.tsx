a1f9b36c108c17166435b07349f56b20cfa15290
achelie <x771364026@gmail.com>
2026-08-19 15:07:35 +0800

    Restore Ahrefs analytics tracking

--- components/site/site-analytics.tsx
+++ components/site/site-analytics.tsx
@@ -1,24 +1,32 @@
 import Script from "next/script";
 
+const AHREFS_ANALYTICS_KEY = "BIpugYY//fqlVBKk5l1Erg";
+
 export function SiteAnalytics() {
   const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-
-  if (!measurementId) {
-    return null;
-  }
 
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        async
+        data-key={AHREFS_ANALYTICS_KEY}
+        src="https://analytics.ahrefs.com/analytics.js"
         strategy="afterInteractive"
       />
-      <Script id="ga4" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
+      {measurementId ? (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga4" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', '${measurementId}', { anonymize_ip: true });`}
-      </Script>
+          </Script>
+        </>
+      ) : null}
     </>
   );
 }
